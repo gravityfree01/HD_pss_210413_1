4f9e1f74ce0582577a3c3fc175c9cae8e9c25a71
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-13 22:26:19 +0900

    1번문제 최종답안 while 문을 이용하여 (어려울땐 for문 별찍기를 기억하자) 1 12 123 1234 123 12 1

--- HD_pss_210413_1/HD_pss_210413_1/Source.cpp
+++ HD_pss_210413_1/HD_pss_210413_1/Source.cpp
@@ -1,79 +1,5 @@
 #include<stdio.h>
-
-/*
-1번문제
-1
-12
-123
-1234
-123
-12
-1
-
-2번문제
-2
-4 6
-8 10 12
-14 16 18 20
-22
-24 26
-28 30 32 
-34 36 38 40
-*/
-
-void main() {
-
-	/*
-	for (int i= 0; i<3; i++){
-
-	printf("hello world");
-
-
-	*/
-	/*int a = 1;
-	while (a < 3) {
-		printf("hello world?");
-		printf("\n");
-		a++;
-	}*/
-
-
-	/*
-
-	int nUm = 1;
-		while (nUm <= 4) {
-			int pnum = 1;
-			while (pnum <= nUm) {
-				printf("%d", pnum);
-				pnum = pnum +1;
-			} nUm++;
-
-			printf("\n");
-		}
-
-	fgetc(stdin);
-
-}
-	*/
-
-
-	/*
-	for (int = i; i < 5; i++) {
-		for (int j = 1; j <= i; j++) {
-			printf("*");
-
-		}
-		pintf("\n");
-	}
-
-	for (int i = 3; i >= 0; i--) {
-
-		for (int k = 1; k <= i; k++) {
-			printf("*")
-		}
-	}
-
-	*/
+void main(){
 
 	int i = 1;
 	while (i < 5) {
@@ -86,7 +12,7 @@ void main() {
 		printf("\n");
 	}
 
-	int i = 3;
+	i = 3;
 	while (i >= 1) {
 		int k = 1;
 		while (k <= i) {
